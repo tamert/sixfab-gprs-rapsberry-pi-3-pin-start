4ab9fea27529d85b379628867737a80f8eb3484d
Tamer Ağaoğlu <farerock@gmail.com>
2018-01-14 19:00:47 +0000

    tutorial start

---
+++ start.py
@@ -0,0 +1,10 @@
+import RPi.GPIO as GPIO ## Import GPIO Library
+import time ## Import ‘time’ library. Allows us to use ‘sleep’
+
+GPIO.setmode(GPIO.BOARD) ## Use BOARD pin numbering
+GPIO.setup(22, GPIO.OUT) ## Setup GPIO pin 7 to OUT
+
+GPIO.output(22, True) ## Turn on GPIO pin 7
+time.sleep(3) ## Wait
+GPIO.output(22, False) ## Switch off GPIO pin 7
+GPIO.cleanup()
